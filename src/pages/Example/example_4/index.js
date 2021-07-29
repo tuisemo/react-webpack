64e25d31d614d9630679037de41c20c4f6389931
tuisemo <tuisemo@sina.cn>
2021-07-29 23:36:54 +0800

    高阶组件实践

--- src/pages/Example/example_4/index.js
+++ src/pages/Example/example_4/index.js
@@ -1,40 +1,23 @@
 import React from 'react';
 import MousePoint from './MousePoint';
-export default class MouseTracker extends React.Component {
-  constructor(props) {
-    super(props);
-    const mockData = new Array(9);
-    this.state = {
-      data: mockData.fill('data')
-    };
-  }
+
+class SpanItem extends React.Component {
   render() {
-    const { data } = this.state;
-    const ListComp = FoldHoc(SpanItem);
+    const { list, handleSetIndex } = this.props;
     return (
       <div>
-        {/* <MousePoint
-          render={state => {
-            return (
-              <div>
-                <span>鼠标横坐标是{state.positionX}</span>
-                <span>鼠标纵坐标是{state.positionY}</span>
-              </div>
-            );
-          }}
-        /> */}
-        <ListComp data={data}></ListComp>
+        {list.map((item, index) => {
+          return (
+            <span key={index} onClick={handleSetIndex(index)}>
+              方块{item}_{index}
+            </span>
+          );
+        })}
       </div>
     );
   }
 }
-/**
- * Render Props
- * React官方给出的定义是：Render Props是指一种在 React 组件之间使用一个值为函数的 prop 共享代码的简单技术
- * 对比props.children,Render Props的优势在于，父组件可以操作子组件的state并作出相应的渲染
- * props.children一般用于样式上的封装
- *
- */
+// 高阶函数——返回一个组件
 const FoldHoc = WrapComponent => {
   return class FoldComp extends React.Component {
     constructor(props) {
@@ -47,10 +30,6 @@ const FoldHoc = WrapComponent => {
     }
     componentDidMount() {
       const { data } = this.props;
-      console.log(
-        '🚀 ~ file: index.js ~ line 51 ~ FoldComp ~ componentDidMount ~ data',
-        data
-      );
       this.setState({
         data: data.slice(0, 5),
         foldState: true,
@@ -90,19 +69,49 @@ const FoldHoc = WrapComponent => {
     }
   };
 };
-class SpanItem extends React.Component {
+// 构造出新组建
+const ListComp = FoldHoc(SpanItem);
+
+export default class MouseTracker extends React.Component {
+  constructor(props) {
+    super(props);
+    const mockData = new Array(9);
+    this.state = {
+      data: mockData.fill('data'),
+      currenIndex: null
+    };
+  }
+  handleSetIndex = index => e => {
+    e.persist();
+    console.log('🚀 ~ file: index.js ~ line 13 ~ MouseTracker ~ index', index);
+    this.setState({
+      currenIndex: index
+    });
+  };
   render() {
-    const { list } = this.props;
+    const { data, currenIndex } = this.state;
     return (
       <div>
-        {list.map((item, index) => {
-          return (
-            <span key={index}>
-              方块{item}_{index}
-            </span>
-          );
-        })}
+        <MousePoint
+          render={state => {
+            return (
+              <div>
+                <span>鼠标横坐标是{state.positionX}</span>
+                <span>鼠标纵坐标是{state.positionY}</span>
+              </div>
+            );
+          }}
+        />
+        <ListComp data={data} handleSetIndex={this.handleSetIndex}></ListComp>
+        <p>{currenIndex}</p>
       </div>
     );
   }
 }
+/**
+ * Render Props
+ * React官方给出的定义是：Render Props是指一种在 React 组件之间使用一个值为函数的 prop 共享代码的简单技术
+ * 对比props.children,Render Props的优势在于，父组件可以操作子组件的state并作出相应的渲染
+ * props.children一般用于样式上的封装
+ *
+ */
