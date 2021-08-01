9454f850a744ff8878f2704bb2550e84f0f39793
tuisemo <tuisemo@sina.cn>
2021-08-01 21:33:40 +0800

    blod学习

--- src/pages/Example/ES6/Blob.js
+++ src/pages/Example/ES6/Blob.js
@@ -5,3 +5,22 @@
  */
 var aBlob = new Blob(blobParts, options);
 //  blobParts：它是一个由ArrayBuffer,ArrayBufferView,Blod,DOMString等对象构成
+
+function dataUrlToBlob(base64, mimeType) {
+  let bytes = window.atob(base64.split(',')[1]);
+  let ab = new ArrayBuffer(bytes.length);
+  let ia = new Uint8Array(ab);
+  for (let i = 0; i < bytes.length; i++) {
+    ia[i] = bytes.charCodeAt(i);
+  }
+  return new Blob([ab], { type: mimeType });
+}
+
+// 保存文件
+function saveFile(blob, filename) {
+  const a = document.createElement('a');
+  a.download = filename;
+  a.href = URL.createObjectURL(blob);
+  a.click();
+  URL.revokeObjectURL(a.href);
+}
